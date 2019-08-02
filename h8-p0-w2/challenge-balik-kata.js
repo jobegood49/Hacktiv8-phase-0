066262d9b0d21eced8a593df012d523000c164f5
jobegood49 <jonathan.s.nicolas@gmail.com>
2019-08-01 20:40:08 -0700

    Add version 1 reverse string algo

---
+++ h8-p0-w2/challenge-balik-kata.js
@@ -0,0 +1,22 @@
+//Version 1
+// function balikKata(kata) {
+//     let revStr = ''
+//     for (let i = kata.length - 1;  i >= 0; i -- ) {
+//         revStr = revStr + kata[i];
+//     }
+//     return revStr
+// }
+
+//Version2
+
+function balikKata(kata) {
+   
+}
+
+// TEST CASES
+console.log(balikKata('John Doe')); // eoD nhoJ
+
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
