f274c20328a6d6be1385853a32f2265a3156990b
jobegood49 <jonathan.s.nicolas@gmail.com>
2019-08-28 19:41:49 -0700

    Add a solution

--- h8-p0-w3/Tentukan-Deret-Aritmatika/Tentukan-Deret-Aritmatika.js
+++ h8-p0-w3/Tentukan-Deret-Aritmatika/Tentukan-Deret-Aritmatika.js
@@ -6,6 +6,22 @@ function tentukanDeretAritmatika(arr) {
   return true;
 }
 
+function tentukanDeretAritmatika(arr) {
+  // Init variables
+  var bedaInit = arr[1] - arr[0];
+  var bedaDeteksi;
+
+  // loop to inspect elements in array
+  for (var i = 0; i < arr.length - 1; i++) {
+    bedaDeteksi = arr[i + 1] - arr[i];
+    // logic to determine if array is indeed arithmetically true
+    if (bedaDeteksi !== bedaInit) {
+      return false;
+    }
+  }
+  return true;
+}
+
 // TEST CASES
 console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
 console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
