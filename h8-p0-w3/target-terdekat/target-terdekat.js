c8b82c8611fc90982903d388a6ae35be75997799
jobegood49 <jonathan.s.nicolas@gmail.com>
2019-08-28 17:39:26 -0700

    Add another solution

--- h8-p0-w3/target-terdekat/target-terdekat.js
+++ h8-p0-w3/target-terdekat/target-terdekat.js
@@ -18,6 +18,28 @@ function targetTerdekat(arr) {
   return Math.min(...distanceArray);
 }
 
+// function targetTerdekat(arr) {
+//     // you can only write your code here!
+//     let position_O = arr.indexOf('o');
+//     let position_X = [];
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] == 'x') {
+//             position_X.push(i);
+//         }
+//     }
+
+//     let resArr = [];
+//     for(let i = 0; i < position_X.length; i++) {
+//         resArr.push(Math.abs(position_X[i] - position_O));
+//     }
+
+//     if(resArr.length == 0) {
+//         return 0
+//     }
+    
+//     return resArr.sort()[0];
+// }
+
 // TEST CASES
 console.log(targetTerdekat([" ", " ", "o", " ", " ", "x", " ", "x"])); // 3
 console.log(targetTerdekat(["o", " ", " ", " ", "x", "x", "x"])); // 4
