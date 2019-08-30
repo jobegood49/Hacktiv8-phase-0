32b00f9f00f958b0203ddb87ad82bb39bb231d24
jobegood49 <jonathan.s.nicolas@gmail.com>
2019-08-29 17:39:20 -0700

    Add another solution

--- h8-p0-w4/tukar-besar-kecil/tukar-besar-kecil.js
+++ h8-p0-w4/tukar-besar-kecil/tukar-besar-kecil.js
@@ -1,20 +1,39 @@
+// function tukarBesarKecil(kalimat) {
+//   // you can only write your code here!
+//   let arr = kalimat.split("");
+//   let newArr = [];
+//   arr.forEach(el => {
+//     if (el.toLowerCase().match(/[a-z]/i)) {
+//       if (el === el.toLowerCase()) {
+//         newArr.push(el.toUpperCase());
+//       } else {
+//         newArr.push(el.toLowerCase());
+//       }
+//     } else {
+//       newArr.push(el);
+//     }
+//   });
+//   return newArr.join("");
+// }
+
+// function tukarBesarKecil(kalimat) {
+//     let newSentence = "";
+//     for (let i = 0; i < kalimat.length; i++) {
+//         newSentence += kalimat[i] !== kalimat[i].toLowerCase() ? kalimat[i].toLowerCase() :
+//         kalimat[i].toUpperCase();
+//     }
+//     return newSentence;
+
+// }
+
 function tukarBesarKecil(kalimat) {
-  // you can only write your code here!
-  let arr = kalimat.split("");
-  let newArr = [];
-  arr.forEach(el => {
-    if (el.toLowerCase().match(/[a-z]/i)) {
-      if (el === el.toLowerCase()) {
-        newArr.push(el.toUpperCase());
-      } else {
-        newArr.push(el.toLowerCase());
-      }
-    } else {
-      newArr.push(el);
-    }
-  });
-  return newArr.join("");
+    return kalimat.split("").reduce((str, char)=> {
+        return str + (char.toLowerCase() === char ? char.toUpperCase(): char.toLowerCase())
+    },"")
+
 }
+
+
 
 // TEST CASES
 console.log(tukarBesarKecil("Hello World")); // "hELLO wORLD"
