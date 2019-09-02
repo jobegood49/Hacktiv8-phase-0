c7cd39f8fdc1bfd1d66364b64d799d955d90a7fe
jobegood49 <jonathan.s.nicolas@gmail.com>
2019-09-02 00:36:55 -0700

    Add solution

--- h8-p0-w4/object-literal/object-literal.js
+++ h8-p0-w4/object-literal/object-literal.js
@@ -1,27 +1,47 @@
+// function changeMe(arr) {
+//   // you can only write your code here!
+//   let currentTime = new Date();
+//   let year = currentTime.getFullYear();
+//   let newArr = [];
+//   arr.forEach(el => {
+//     let age = 0;
+//     if (year > el[3]) {
+//       age = year - el[3];
+//     } else {
+//       age = "Invalid Birth Year";
+//     }
+
+//     newArr.push({
+//       firstName: el[0],
+//       lastName: el[1],
+//       gender: el[2],
+//       age: age
+//     });
+//   });
+
+//   console.log(newArr);
+
+//   return newArr;
+// }
+
 function changeMe(arr) {
-  // you can only write your code here!
-  let currentTime = new Date();
-  let year = currentTime.getFullYear();
-  let newArr = [];
-  arr.forEach(el => {
-    let age = 0;
-    if (year > el[3]) {
-      age = year - el[3];
+  const currentTime = new Date();
+  const year = currentTime.getFullYear();
+  const people = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let person = {};
+    person.firstName = arr[i][0];
+    person.lastName = arr[i][1];
+    person.gender = arr[i][2];
+    if (arr[i][3] > year || arr[i][3] === undefined) {
+      person.age = "Invalid Birth Year";
     } else {
-        age = 'Invalid Birth Year' 
+      person.age = year - arr[i][3];
     }
-
-    newArr.push({
-      firstName: el[0],
-      lastName: el[1],
-      gender: el[2],
-      age: age
-    });
-  });
-  
-  console.log(newArr);
-
-  return newArr;
+    people.push(person);
+  }
+  return people;
 }
 
 // TEST CASES
@@ -35,4 +55,4 @@ changeMe([["Christ", "Evans", "Male", 1982], ["Robert", "Downey", "Male"]]); // 
 //   lastName: 'Downey',
 //   gender: 'Male',
 //   age: 'Invalid Birth Year' }
-changeMe([]); // ""
+// changeMe([]); // ""
